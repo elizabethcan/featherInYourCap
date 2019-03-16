3f03d24d17c0ba91dfbfaa97d043f31aaabe83d5
Elizabeth Cannon <elizabeth230@gmail.com>
2019-03-16 13:18:00 -0700

    Add form for travel

--- client/src/components/Travel.jsx
+++ client/src/components/Travel.jsx
@@ -4,8 +4,24 @@ const Travel = (props) => {
   if (props.view === 'travelGoal') {
     return (
       <div>
-        <div>Travel</div>
-        
+        <div>Where would you like to travel?</div>
+        <select>
+          <option>Bali(Indonesia)</option>
+          <option>Costa Rica</option>
+          <option>France</option>
+          <option>Japan</option>
+          <option>Spain</option>
+          <option>Thailand</option>
+        </select>
+        <div>What type of budget would you want?</div>
+        <select>
+          <option>Ballin' on a Budget</option>
+          <option>Livin' Like a King</option>
+          <option>Somewhere in Between</option>
+        </select>
+        <div>How long do you want to go for?</div>
+        <input></input>
+        <button>Let's Do It!</button>
       </div>
     )
   } else {
