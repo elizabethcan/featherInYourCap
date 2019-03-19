d18fa96028b2cb4af1b7f4e4347aaa07e3cc25ef
Elizabeth Cannon <elizabeth230@gmail.com>
2019-03-18 18:51:09 -0700

    remove unused server code

--- server/index.js
+++ server/index.js
@@ -8,18 +8,9 @@ const port = 3000;
 
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
-app.get('/countries/:countryId', (req, res) => {
-  console.log(`countryId`, req.params.countryId);
-  country.getCountry(parseInt(req.params.countryId), (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send(err);
-    } else {
-      console.log(data);
-      res.status(200).send(data);
-    }
-  });
-});
+app.get('/countries', (req, res) => {
+
+})
 
 app.listen(port, 'localhost', () => {
   console.log(`Now listening on port ${port}`);
