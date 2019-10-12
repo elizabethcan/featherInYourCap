e7618f69b546d3571ec333869c345bed0dc50048
Elizabeth Cannon <elizabeth230@gmail.com>
2019-10-12 15:19:46 -0700

    removing config since not used

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const request = require('request');
 const db = require('../database/index.js');
 const country = require('../database/controllers/country.js');
-const headers = require('../axios.config.js').headers;
 
 const port = 3000;
 
@@ -14,7 +13,6 @@ app.use(express.static(path.resolve(__dirname, '../client/dist')));
 app.get('/countries', (req, res) => {
   let options = {
     url: 'https://www.budgetyourtrip.com/api/v3/countries',
-    headers: headers
   }
   request(options, (error, response, body) => {
     if (error) {
@@ -32,7 +30,6 @@ app.get('/convert', (req, res) => {
   let to = req.query.to;
   let options = {
     url: `https://www.budgetyourtrip.com/api/v3/currencies/convert/${from}/${to}`,
-    headers: headers
   }
   request(options, (error, response, body) => {
     if (error) {
